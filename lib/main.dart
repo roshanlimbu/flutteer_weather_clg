a1f6f6c03f9111e14958aba8ecb34a8fc3f4b06d
roshanlimbu <topazkayle@gmail.com>
2025-03-07 08:09:19 +0545

    init codes

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:day4/features/home/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Weather App",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
+    );
+  }
+}
